4fe8c57d334fbd45e9839121d55ddfa01fb53696
goat3ggs <stefpandi12@gmail.com>
2026-04-29 20:47:40 +0300

    Add scroll event listener for sticky nav

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const nav = document.querySelector(".nav");
 const iconOpen = document.querySelector(".header__burger--open");
 const iconClose = document.querySelector(".header__burger--close");
 const navLinks = document.querySelectorAll(".nav__link");
-
+const header = document.querySelector(".header");
 
 burerBtn.addEventListener("click", () => {
     nav.classList.toggle("nav--hidden");
@@ -16,11 +16,19 @@ burerBtn.addEventListener("click", () => {
 
 navLinks.forEach((el) => {
     el.addEventListener("click", () => {
-        nav.classList.add("u-hidden");
+        nav.classList.add("nav--hidden");
         iconClose.classList.add("u-hidden");
         iconOpen.classList.remove("u-hidden");
 
         document.documentElement.classList.remove("u-no-scroll"); 
         document.body.classList.remove("u-no-scroll");
     });
-})
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("header--sticky");
+    } else {
+        header.classList.remove("header--sticky");
+    }
+});
