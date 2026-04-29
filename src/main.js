27722a96bed29a4eba63fced84390fc1d8e96c6a
goat3ggs <stefpandi12@gmail.com>
2026-04-29 13:06:15 +0300

    initialize the structure of the website and add general styling

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const burerBtn = document.querySelector(".header__burger");
+const nav = document.querySelector(".nav");
+const iconOpen = document.querySelector(".header__burger--open");
+const iconClose = document.querySelector(".header__burger--close");
+const navLinks = document.querySelectorAll(".nav__link");
+
+
+burerBtn.addEventListener("click", () => {
+    nav.classList.toggle("nav--hidden");
+    iconOpen.classList.toggle("u-hidden");
+    iconClose.classList.toggle("u-hidden");
+
+    document.documentElement.classList.toggle("u-no-scroll"); 
+    document.body.classList.toggle("u-no-scroll");
+});
+
+navLinks.forEach((el) => {
+    el.addEventListener("click", () => {
+        nav.classList.add("u-hidden");
+        iconClose.classList.add("u-hidden");
+        iconOpen.classList.remove("u-hidden");
+
+        document.documentElement.classList.remove("u-no-scroll"); 
+        document.body.classList.remove("u-no-scroll");
+    });
+})
